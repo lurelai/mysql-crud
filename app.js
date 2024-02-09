bdd5483ba7b570f1695d689e14970d267485f780
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-09 15:18:28 -0300

    connect with database

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
 // routes
 require('./src/Routes/userRoute')(app)
+
 
 app.get('/', (req, res)=>{
     res.send('A simple crud using mysql-database and mysql package')
